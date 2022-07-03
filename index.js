592b63d1edfa6c42832b0e642cad595f02d42bf1
Aline Bury <alinebury@gmail.com>
2022-07-03 18:36:19 -0300

    Endpoint POST /login

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
+const tokenGenerator = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 16; i += 1) {
+      result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+};
+
 app.get('/talker', (req, res) => {
   const file = fs.readFileSync(filePath, 'utf8');
   const parsed = JSON.parse(file);
@@ -35,4 +44,9 @@ app.get('/talker/:id', (req, res) => {
   if (!item) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(200).json(item);
+});
+
+app.post('/login', (req, res) => {
+  const token = tokenGenerator();
+  res.status(200).json({ token });
 });
